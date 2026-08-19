983c766e640fb60ceb8d97b2e632fad74cc88138
pieman492 <rauljrrauljr@gmail.com>
2026-08-19 03:01:55 -0500

    what the hell

--- dev/scripts/NeoVariantusDeletus/Typelists.py
+++ dev/scripts/NeoVariantusDeletus/Typelists.py
@@ -1,1 +1,33 @@
-/home/pieman492/SSDInstancesPrism/Strange Kitchen Sink/.minecraft/DevScripts/Typelists.py
+colors = [ 
+    "white",
+    "light_gray",
+    "gray",
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "lime",
+    "green",
+    "cyan",
+    "light_blue",
+    "blue",
+    "purple",
+    "magenta",
+    "pink"
+]
+
+vanilla_woods = [
+    "oak",
+    "spruce",
+    "birch",
+    "jungle",
+    "acacia",
+    "dark_oak",
+    "mangrove",
+    "cherry",
+    "bamboo",
+    "crimson",
+    "warped"
+]
+
